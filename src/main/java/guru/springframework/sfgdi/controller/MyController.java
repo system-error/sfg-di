77b29deaed241d321e051ebfa449be8515a0e6e5
system-error <christos_dousmanopoulos@yahoo.gr>
2021-02-03 14:27:53 +0200

    Initial Commit

---
+++ src/main/java/guru/springframework/sfgdi/controller/MyController.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgdi.controller;
+
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    private final PrimaryGreetingService primaryGreetingService;
+
+    public MyController(PrimaryGreetingService primaryGreetingService) {
+        this.primaryGreetingService = primaryGreetingService;
+    }
+
+    public String sayHello(){
+
+        return primaryGreetingService.sayGreeting();
+    }
+}
